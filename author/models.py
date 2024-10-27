6935e82fcfeacdc637d132f0c85aa6007193c665
officialalkenes <belloabdulhakeemolamide@gmail.com>
2024-10-27 07:29:01 +0100

    Basic Model Setup part 1

--- author/models.py
+++ author/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from django.utils.text import slugify
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pics = models.ImageField(upload_to="author_profile_pics")
+    pseudonym = models.CharField(max_length=100, unique=True) # My boy
+    slug = models.SlugField(max_length=100, blank=True) # my-boy
+    website = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.pseudonym}'
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.pseudonym)
+        return super().save(*args, **kwargs)
--- bookapp/models.py
+++ bookapp/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.utils.text import slugify
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(blank=True, max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
